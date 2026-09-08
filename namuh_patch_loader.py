d0cf6f2c817fd0ae8a6cb57907538ea0597fd2d7
zzzgnt-a11y <zzzgnt@gmail.com>
2026-09-08 15:02:25 +0900

    Load stock-only scalp diversity patch last

--- namuh_patch_loader.py
+++ namuh_patch_loader.py
@@ -94,6 +94,7 @@ def install():
                         import namuh_stability70_final;namuh_stability70_final.apply(ns)
                         import namuh_investor_probe_0908;namuh_investor_probe_0908.apply(ns)
                         import namuh_scalp_sector_final_0908;namuh_scalp_sector_final_0908.apply(ns)
+                        import namuh_scalp_sector_diversity_v2;namuh_scalp_sector_diversity_v2.apply(ns)
                 except Exception as exc:
                     print('NAMUH USER15 LATE PATCH ERROR:',str(exc)[:220],flush=True)
                 return _prev_run(*args,**kwargs)
